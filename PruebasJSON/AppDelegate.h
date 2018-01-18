d9a53ba973b49273176331a654fadd0985ce3570
cice <cice@aula1povm22-10.local>
2018-01-18 18:09:43 +0100

    Initial Commit

---
+++ PruebasJSON/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PruebasJSON
+//
+//  Created by cice on 18/1/18.
+//  Copyright © 2018 TATINC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PruebasJSON/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PruebasJSON
+//
+//  Created by cice on 18/1/18.
+//  Copyright © 2018 TATINC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
